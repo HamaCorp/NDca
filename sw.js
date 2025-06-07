b816c9d970261c59a190f9b612a700668bfd65c7
hama-171 <o-hama4@vanilla.ocn.ne.jp>
2025-06-07 21:19:37 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'nd-timer-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Service Workerのインストール
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// リクエストの制御 (キャッシュ優先)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // キャッシュがあればそれを返す
+        if (response) {
+          return response;
+        }
+
+        // キャッシュがない場合、通信が発生することをページに通知
+        self.clients.matchAll().then(clients => {
+          clients.forEach(client => {
+            client.postMessage({ type: 'NETWORK_ACCESS' });
+          });
+        });
+
+        // 通信してリソースを取得し、キャッシュに保存する
+        return fetch(event.request).then(
+          networkResponse => {
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+            const responseToCache = networkResponse.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return networkResponse;
+          }
+        );
+      })
+  );
+});
+
+// 古いキャッシュの削除
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
